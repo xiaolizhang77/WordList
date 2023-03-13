ebd80fadade6191acc9ef3327cb21ec588b18dab
Lenovo <987507935@qq.com>
2023-03-13 11:16:33 +0800

    add function3 combine function4

--- task3.cpp
+++ task3.cpp
@@ -58,3 +58,68 @@ void longest_chain_char(int edge[26][26], int *length, char *nodes) {
     free(lengths);
     free(prevs);
 }
+
+void
+dfs(int edge[26][26], int finalPath[], int *nFinalPath, int path[], int *nPath, char now, char last, bool self,
+    int *finalLength, int *length) {
+    if (self && edge[now - 'a'][now - 'a'] > 0) {
+        path[*nPath] = now - 'a';
+        *nPath = *nPath + 1;
+        *length = *length + edge[now - 'a'][now - 'a'];
+        dfs(edge, finalPath, nFinalPath, path, nPath, now, last, false, finalLength, length);
+        *length = *length - edge[now - 'a'][now - 'a'];
+        *nPath = *nPath - 1;
+    } else {
+        if (now == last) {
+            if (*length > *finalLength) {
+                *finalLength = *length;
+                *nFinalPath = *nPath;
+                int i;
+                for (i = 0; i < *nPath; i++) {
+                    finalPath[i] = path[i];
+                }
+            }
+            return;
+        }
+        int i;
+        for (i = 0; i < 26; i++) {
+            if (i != now - 'a' && edge[now - 'a'][i] > 0) {
+                path[*nPath] = i;
+                *nPath = *nPath + 1;
+                *length = *length + edge[now - 'a'][i];
+                dfs(edge, finalPath, nFinalPath, path, nPath, i + 'a', last, true, finalLength, length);
+                *length = *length - edge[now - 'a'][i];
+                *nPath = *nPath - 1;
+            }
+        }
+    }
+}
+
+#include "stdio.h";
+
+void function3And4(int edge[26][26], char first, char last, int *n, int *answer, int *nLength) {
+    int finalPath[100];
+    int nFinalPath = 0;
+    int finalLength = 0;
+    int path[100];
+    path[0] = first - 'a';
+    int nPath = 1;
+    int length = 0;
+    dfs(edge, finalPath, &nFinalPath, path, &nPath, first, last, true, &finalLength, &length);
+    if (nFinalPath > 1) {
+//        printf("%c %c %d:", first, last, nFinalPath);
+//        int i;
+//        for (i = 0; i < nFinalPath; i++) {
+//            printf("%c ", finalPath[i] + 'a');
+//        }
+//        printf("\n");
+        *n = nFinalPath;
+        *nLength = finalLength;
+        int i;
+        for (i = 0; i < *n; i++) {
+            *(answer + i) = finalPath[i];
+        }
+        return;
+    }
+    *n = 0;
+}
