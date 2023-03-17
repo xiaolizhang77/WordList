f62c902ba8468e2500405c41e12573f1425c079b
Lenovo <987507935@qq.com>
2023-03-17 17:18:16 +0800

    测试模板

--- test/test_wordlist.cpp
+++ test/test_wordlist.cpp
@@ -5,7 +5,7 @@
 #include "word_list.h" // 如果你有一个头文件定义了libword_list.dll中的接口，请在此处包含
 
 int main() {
-    std::cout << "开始测试libword_list.dll..." << std::endl;
+    std::cout << "test start..." << std::endl;
 
     // 从文件中读取字符串
     char **strings;
@@ -16,7 +16,7 @@ int main() {
     gen_chains_all(strings, n, result);
 
     // 测试完成
-    std::cout << "测试libword_list.dll完成！" << std::endl;
+    std::cout << "test end！" << std::endl;
 
     return 0;
 }
